28d0d3840dfd3f78303ad0bc1511b092799e5129
exaul <137360873+exaul@users.noreply.github.com>
2024-02-23 21:27:13 -0500

    Agregar archivos de proyecto.

---
+++ Creditos_Ecomicos_2024/Controllers/ClientesController.cs
@@ -0,0 +1,51 @@
+﻿using Creditos_Ecomicos_2024.Data;
+using Creditos_Ecomicos_2024.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Creditos_Ecomicos_2024.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientesController : ControllerBase
+    {
+        private readonly CreditosDbContext creditosDb;
+
+        public ClientesController(CreditosDbContext creditosDb)
+        {
+            this.creditosDb = creditosDb;
+        }
+        // GET: api/<ClientesController>
+        [HttpGet]
+        public IEnumerable<Cliente> Get()
+        {
+            return creditosDb.Clientes.ToList();
+        }
+
+        // GET api/<ClientesController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ClientesController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ClientesController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ClientesController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
